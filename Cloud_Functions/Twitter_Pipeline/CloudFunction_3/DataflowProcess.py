2637fe8e1fa7164c6bcbc0cf6d3a00c6d55146f3
Sergio Duran <64727841+sduransp@users.noreply.github.com>
2021-02-24 11:15:16 +0100

    Add files via upload

---
+++ Cloud_Functions/Twitter_Pipeline/CloudFunction_3/DataflowProcess.py
@@ -0,0 +1,38 @@
+def startDataflowProcess2(data, context):
+     from googleapiclient.discovery import build
+     #replace with your projectID
+     project = 'iwine-tfm'
+     #job = project + " " + str(data['timeCreated'])
+     time1 = str(data['timeCreated'])
+     time1.replace(":","-")
+     time1.replace(".","-")
+     job = project + "_" + time1
+     #path of the dataflow template on google storage bucket
+     template = 'gs://dataflow-templates-us-central1/latest/GCS_Text_to_BigQuery'
+     inputFile = 'gs://tweets-ml/' + str(data['name'])
+     #user defined parameters to pass to the dataflow pipeline job
+     parameters = {
+          'javascriptTextTransformFunctionName': 'transform',
+          'javascriptTextTransformGcsPath':'gs://ml-flow-iwine/transform.js',
+          'JSONPath': 'gs://ml-flow-iwine/Schema.json',
+          'inputFilePattern': inputFile,
+          'outputTable': 'iwine-tfm:ml_insights.tweets',
+          'bigQueryLoadingTemporaryDirectory':'gs://ml-flow-iwine/temp/BQ_temp'
+     }
+     #tempLocation is the path on GCS to store temp files generated during the dataflow job
+     environment = {'tempLocation': 'gs://dataflow-twapp/BQ_temp/temp'}
+
+     service = build('dataflow', 'v1b3', cache_discovery=False)
+     #below API is used when we want to pass the location of the dataflow job
+     request = service.projects().locations().templates().launch(
+          projectId=project,
+          gcsPath=template,
+          location='us-central1',
+          body={
+               'jobName': job,
+               'parameters': parameters,
+               'environment':environment
+          },
+     )
+     response = request.execute()
+     print(str(response))
